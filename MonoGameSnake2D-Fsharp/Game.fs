42fdb30ee30b5f6b4471e295f641c24b8e27cce6
jonashw <2jonwilson@gmail.com>
2017-10-16 14:04:58 -0700

    developed rendering strategy, supported by comments and the PixelPerfectRenderer class

--- MonoGameSnake2D-Fsharp/Game.fs
+++ MonoGameSnake2D-Fsharp/Game.fs
@@ -4,14 +4,16 @@ open Microsoft.Xna.Framework.Input
 open Microsoft.Xna.Framework.Graphics
 open PalleteColor
 open Movement
- 
+open PixelPerfectRenderer
+
 type Game1 () as x =
     inherit Game()
- 
     do x.Content.RootDirectory <- "Content"
+    do x.Window.Title <- "Snake!"
     let graphics = new GraphicsDeviceManager(x)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
     let mutable pixelTexture: Texture2D = null
+    let mutable pixelRenderer = Unchecked.defaultof<PixelPerfectRenderer>
     let mutable snake = 
         Snake.makeSnake
             (60,5)
@@ -26,7 +28,7 @@ type Game1 () as x =
                 ]
         List.concat 
             [
-                makePerimeter ( 0, 0) (79,47) //level border
+                makePerimeter ( 0, 0) (79,44) //level border
                 makePerimeter (20,10) (59,35) //inner cloister
             ]
     let wormholes = 
@@ -47,19 +49,31 @@ type Game1 () as x =
             (Reflect)
         ]
     let mutable nextHeading = None
-    let mutable vp: Viewport = Unchecked.defaultof<Viewport>
     let backgroundColor = Blank, Lowest
  
     override x.Initialize() =
+        (* Let it be known that from now on,
+        ** this game's display will adhere to a 16:9 aspect ratio in landscape orientation 
+        ** and a 9:16 aspect ratio in portrait orientation.
+        ** All levels will be designed with this in mind.
+        ** Furthermore, all levels will be playable in either landscape or portrait orientation.
+        ** A transition from one orientation to the other mid-game should not affect gameplay in any way;
+        ** the player will simply have a new perspective on the current level. 
+        ** For now, let's use a fixed level size of 80 tiles x 45 tiles *)
+        let aspectRatio = 16.0f / 9.0f
+        let getScreenResolutionByWidth width = 
+            let height = (float32 width) / aspectRatio |> int
+            {Width = width; Height = height}
         spriteBatch <- new SpriteBatch(graphics.GraphicsDevice)
-        vp <- graphics.GraphicsDevice.Viewport
+        pixelRenderer <- new PixelPerfectRenderer(
+            graphics,
+            (getScreenResolutionByWidth 800), 
+            (getScreenResolutionByWidth 2000))
         base.Initialize()
-        ()
  
     override x.LoadContent() =
         do pixelTexture <- new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color)
         pixelTexture.SetData([| Color.White |])
-        ()
  
     override x.Update (gameTime) =
         let kb = Keyboard.GetState()
@@ -84,6 +98,7 @@ type Game1 () as x =
         do snake <- Snake.update snake nextGridOkHeading newPowerUp teleport (gameTime.ElapsedGameTime) 
  
     override x.Draw (gameTime) =
+        pixelRenderer.Begin()
         do x.GraphicsDevice.Clear <| toColor backgroundColor
         spriteBatch.Begin()
         do Snake.draw spriteBatch pixelTexture snake
@@ -92,3 +107,4 @@ type Game1 () as x =
         for b in blocks do
             Block.draw spriteBatch pixelTexture b
         spriteBatch.End()
+        pixelRenderer.End()
---
+++ MonoGameSnake2D-Fsharp/PixelPerfectRenderer.fs
@@ -0,0 +1,38 @@
+﻿module PixelPerfectRenderer
+
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework
+
+type ScreenResolution = { Width: int; Height: int }
+
+(* Inspirations for this implementation:
+**      - "Scaling entire screen in XNA" [https://stackoverflow.com/a/7603116/943730]
+**      - "What is the best Screen Resolution for pixel art 2d game." [http://community.monogame.net/t/what-is-best-screen-resolution-for-pixel-art-2d-game/7876]
+*)
+type PixelPerfectRenderer
+    ( graphics: GraphicsDeviceManager
+    , nativeResolution: ScreenResolution
+    , finalDisplayResolution: ScreenResolution) =
+    let device = graphics.GraphicsDevice
+    let renderTarget = new RenderTarget2D(device, nativeResolution.Width, nativeResolution.Height)
+    let scaleBatch = new SpriteBatch(device)
+    let finalDisplayViewport = new Rectangle(0, 0, finalDisplayResolution.Width, finalDisplayResolution.Height)
+    do
+        graphics.PreferredBackBufferWidth <- finalDisplayResolution.Width
+        graphics.PreferredBackBufferHeight <- finalDisplayResolution.Height
+        graphics.ApplyChanges()
+    member x.Begin () =
+        device.SetRenderTarget(renderTarget)
+    member x.End () =
+        device.SetRenderTarget(null)
+        scaleBatch.Begin(
+            SpriteSortMode.Deferred,
+            BlendState.NonPremultiplied,
+            SamplerState.PointClamp,
+            DepthStencilState.Default,
+            RasterizerState.CullNone)
+        scaleBatch.Draw(
+            renderTarget, 
+            finalDisplayViewport,
+            Color.White)
+        scaleBatch.End()
