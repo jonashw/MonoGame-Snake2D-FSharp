37eba4fbb4950384e910585dcb6d557be2aaced6
jonashw <2jonwilson@gmail.com>
2017-11-05 19:35:08 -0800

    got first unit test to run (and pass!)

--- CoreUnitTests/Example.fs
+++ CoreUnitTests/Example.fs
@@ -1,12 +1,12 @@
 ﻿module Example
 
 open NUnit.Framework
-open FsUnit
 open Snake
 
 [<TestFixture>]
 type Example() =
     let s = Snake.makeSnake (0,0) (Movement.X, Movement.Positive) 5 0.0f
+
     [<Test>]
     member x.``is digital by default`` () =
-        Snake.isTileDigital s |> should equal true
+        Assert.True(Snake.isTileDigital s)
